b82a72165c9f0a3e8d3098f9fcf46d210a4d3d90
ruttklein <klein.rutto@student.moringaschool.com>
2023-09-20 11:08:40 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const divide = (num1="2000",num2="100") => num1 / num2;
+divide();
+
+const square = x => x*x;
+square();
+
+const add =( parameter1,parameter2) => parameter1 + parameter2;
+add(12,7);
